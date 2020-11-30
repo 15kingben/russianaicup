64f00128e9cfefb09cadc774caf7349b1e08de0f
15kingben <bking2015@gmail.com>
2020-11-29 21:56:36 -0800

    noot noot

--- codecraft/cpp/MyStrategy.cpp
+++ codecraft/cpp/MyStrategy.cpp
@@ -19,7 +19,7 @@ void oneTimeInitialization();
 
 std::vector<std::vector<bool> > open;
 bool oneTimeInitDone = false;
-PlayerView &pv;
+PlayerView &pv = nullptr;
 
 enum BuilderStrat {
     MINE = 0,
