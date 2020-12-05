e01abbfde97f70c8faddd6d9ad75bd4074ecd63b
15kingben <bking2015@gmail.com>
2020-12-04 21:43:03 -0800

    noot noot

--- codecraft/cpp/ArmyManager.cpp
+++ codecraft/cpp/ArmyManager.cpp
@@ -95,6 +95,7 @@ EntityAction ArmyManager::getDefendAction(CombatUnit & unit) {
     if (unit.fallback) {
         if (Util::dist2(unit.entity.position, Util::homeBase) < Util::dist2(Vec2Int(0,0), Vec2Int(RECOVER_DISTANCE, RECOVER_DISTANCE))) {
             unit.fallback = false;
+            std::cout << "reset" << std::endl;
         } else {
             return action;
         }
