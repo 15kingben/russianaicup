87e99d3a6143ffa75a7fe3704f37b4af70a95d95
15kingben <bking2015@gmail.com>
2020-12-06 20:09:50 -0800

    noot noot

--- codecraft/cpp/MyStrategy.cpp
+++ codecraft/cpp/MyStrategy.cpp
@@ -185,7 +185,10 @@ void oneTimeInitialization() {
     Util::myId = pv->myId;    
 
     for (auto entity : pv->entities) {
-        if (entity.entityType == BUILDER_BASE && *entity.playerId.get() == pv->myId) Util::homeBase = entity.position;
+        if (entity.entityType == BUILDER_BASE) {
+            std::cout << "poopoo" << entity.playerId << std::endl;
+        }
+        if (entity.entityType == BUILDER_BASE && *(entity.playerId.get()) == pv->myId) Util::homeBase = entity.position;
     }
 
     armyManager.setMaxDistance(pv->mapSize);
