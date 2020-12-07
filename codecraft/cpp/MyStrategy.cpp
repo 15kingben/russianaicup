4e1843a32f978d311085a0eeda9e1ccba2e82d74
15kingben <bking2015@gmail.com>
2020-12-06 20:21:54 -0800

    noot noot

--- codecraft/cpp/MyStrategy.cpp
+++ codecraft/cpp/MyStrategy.cpp
@@ -154,7 +154,7 @@ void everyTickInitialization() {
         if (pv->players[i].id == pv->myId) 
             break;
 
-    economy.setResources(pv->players[i].resource);
+    economy.setResources(pv->players[pv->myId].resource);
     economy.setPopulation(0);
     Util::entities.clear();
     for (Entity e : pv->entities) {
