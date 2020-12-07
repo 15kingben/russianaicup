1bae4a66bb22ee7fbe574b13d123f3bf6855f0a7
15kingben <bking2015@gmail.com>
2020-12-06 20:05:50 -0800

    noot noot

--- codecraft/cpp/Util.cpp
+++ codecraft/cpp/Util.cpp
@@ -117,6 +117,7 @@ Vec2Int Util::getHomeDirection() {
     int ms = Util::mapSize;
     int x = hb.x < ms / 2 ? -1 : 1;
     int y = hb.y < ms / 2 ? -1 : 1;
+    std::cout << Util::homeBase.x << " " << Util::homeBase.y << " " << x " " << y << std::endl;
     return Vec2Int(x, y);
 }
 
