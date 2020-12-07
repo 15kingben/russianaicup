5cef7a2d65fcf7af758441ddb49467bdbb8eab57
15kingben <bking2015@gmail.com>
2020-12-06 20:51:47 -0800

    noot noot

--- codecraft/cpp/BuilderManager.cpp
+++ codecraft/cpp/BuilderManager.cpp
@@ -242,12 +242,15 @@ int BuilderManager::getCommitted() {
 int MAX_REPAIR_CT = 2;
 
 void BuilderManager::repair(Repairable& entity, std::vector<std::vector<Square> > & open) {
-    for (int i = 0; i < 1000; i++) {
-        std::cout << "AHHHHHHHHHHHHHH" << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "yes" << std::endl;
     }
     if (entity.getEntity().health == Util::entityProperties[entity.getEntity().entityType].maxHealth) {
         entity.helpers.clear();
         return;
+    }
+    for (int i = 0; i < 5; i++) {
+        std::cout << "no" << std::endl;
     }
 
     int x = (getBuilderCount() / 5) - getCommitted();
